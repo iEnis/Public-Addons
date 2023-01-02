1d99e600242cbab78a94eef93d00b3326907433a
IEnis <enis-neimi@outlook.de>
2023-01-02 17:20:56 +0100

    Updated for better performance

--- xyz-to-score/scripts/index.js
+++ xyz-to-score/scripts/index.js
@@ -1,22 +1,17 @@
-import { world } from '@minecraft/server'
+import { world, system } from '@minecraft/server'
 import * as config from './config'
 
+world.events.worldInitialize.subscribe(() => {
+    try { world.scoreboard.addObjective(config.x, config.x) } catch (e) {}
+    try { world.scoreboard.addObjective(config.y, config.y) } catch (e) {}
+    try { world.scoreboard.addObjective(config.z, config.z) } catch (e) {}
+})
+
 world.events.tick.subscribe(() => {
-    try { world.getDimension("overworld").runCommand(`scoreboard objectives add ${config.x} dummy`) } catch (e) {}
-    try { world.getDimension("overworld").runCommand(`scoreboard objectives add ${config.y} dummy`) } catch (e) {}
-    try { world.getDimension("overworld").runCommand(`scoreboard objectives add ${config.z} dummy`) } catch (e) {}
-
-    const players = Array.from(world.getPlayers())
-    players.forEach((player) => {
-        let x = parseInt(`${player.location.x}`.split(".")[0])
-        if (player.location.x < 0) { x = x - 1 }
-        let y = parseInt(`${player.location.y}`.split(".")[0])
-        if (y < 0) { y = y - 1 }
-        let z = parseInt(`${player.location.z}`.split(".")[0])
-        if (player.location.z < 0) { z = z - 1 }
-
-        player.runCommand(`scoreboard players set @s ${config.x} ${x}`)
-        player.runCommand(`scoreboard players set @s ${config.y} ${y}`)
-        player.runCommand(`scoreboard players set @s ${config.z} ${z}`)
+    Array.from(world.getPlayers()).forEach((player) => {
+        let {x, y, z} = player.location
+        player.runCommandAsync(`scoreboard players set @s ${config.x} ${Math.floor(x)}`)
+        player.runCommandAsync(`scoreboard players set @s ${config.y} ${Math.floor(y)}`)
+        player.runCommandAsync(`scoreboard players set @s ${config.z} ${Math.floor(z)}`)
     })
 })
